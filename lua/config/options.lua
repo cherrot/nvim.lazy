c9d56a61ed17cb3b71c22e6b56fb3ebaba57fc38
cherrot <cherrotluo@gmail.com>
2024-09-29 13:41:28 +0800

    feat: go through lazyvim.org

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,11 @@
 
 -- Display file path and modifiy status of the buffer (top-right corner)
 vim.opt.winbar = "%=%m %f"
+
+-- https://www.lazyvim.org/extras/lang/python
+--
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -13,37 +13,6 @@ return {
     },
   },
 
-  -- add more treesitter parsers
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-
-        "css",
-        "make",
-        "go",
-        "gomod",
-        "rust",
-        "vue",
-        "svelte",
-      },
-    },
-  },
-
   -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
   -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
   { import = "lazyvim.plugins.extras.lang.typescript" },
@@ -51,24 +20,55 @@ return {
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
 
+  -- https://www.lazyvim.org/configuration/recipes#use-eslint-for-fix-on-save-and-prettier-for-formatting
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+
+  -- https://www.lazyvim.org/extras/coding/copilot
+  { import = "lazyvim.plugins.extras.coding.copilot" },
+  -- https://www.lazyvim.org/extras/lang/go
+  { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.helm" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  -- https://www.lazyvim.org/extras/lang/python
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.svelte" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.toml" },
+  { import = "lazyvim.plugins.extras.lang.vue" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+  -- https://www.lazyvim.org/extras/lsp/none-ls
+  -- { import = "lazyvim.plugins.extras.lsp.none-ls" },
+
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "make",
+      })
+    end,
+  },
+
   -- add any tools you want to have installed below
   {
     "williamboman/mason.nvim",
     opts = {
+      ui = { border = "rounded" },
       ensure_installed = {
         "stylua",
         "shellcheck",
         "shfmt",
-        "flake8", -- Python
-        "delve", -- Go
-        "bash-language-server", -- "bashls",
+        -- "flake8", -- Python
+        -- "bash-language-server", -- "bashls",
         -- "clangd",
-        "html-lsp", -- "html",
-        "json-lsp", -- "jsonls",
-        "lua-language-server", -- "lua_ls",
-        "python-lsp-server", -- pylsp
-        "gopls",
-        "helm-ls",
+        -- "html-lsp", -- "html",
+        -- "json-lsp", -- "jsonls",
+        -- "lua-language-server", -- "lua_ls",
+        -- "python-lsp-server", -- pylsp
+        -- "helm-ls",
+        "buf-language-server",
       },
     },
   },
