d7adee58c36f0cd4eee55e77994ab00cd841a186
cherrot <cherrotluo@gmail.com>
2025-04-23 15:15:40 +0800

    feat(ai): use native claude API

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -28,16 +28,42 @@ return {
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
     opts = {
-      provider = "openai",
+      provider = "claude",
       auto_suggestions_provider = "openai",
+
       openai = {
         endpoint = os.getenv("OPENAI_ENDPOINT") .. "/v1",
-        api_key_name = "OPENAI_API_KEY", -- the shell command must prefixed with `^cmd:(.*)`
-        -- model = "claude-3-5-sonnet-20240620",
-        model = "claude-3-5-sonnet-20241022",
-        -- max_tokens = 8192,
+        model = "gpt-4o",
+        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+        temperature = 0,
+        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        -- reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
       },
+
+      claude = {
+        endpoint = "https://aihubmix.com",
+        model = "claude-3-7-sonnet-20250219",
+        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+        temperature = 0,
+        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+      },
+      -- -- these tools may conflict with mcp-hub
+      -- disabled_tools = {
+      --   "list_files",
+      --   "search_files",
+      --   "read_file",
+      --   "create_file",
+      --   "rename_file",
+      --   "delete_file",
+      --   "create_dir",
+      --   "rename_dir",
+      --   "delete_dir",
+      --   "bash",
+      -- },
       behaviour = {
+        enable_cursor_planning_mode = true, -- Whether to enable Cursor Planning Mode. Default to false.
+        enable_claude_text_editor_tool_mode = true, -- claude only
+
         auto_suggestions = true, -- Experimental stage
         auto_set_highlight_group = true,
         auto_set_keymaps = true,
@@ -50,6 +76,19 @@ return {
         -- Options override for custom providers
         provider_opts = {},
       },
+
+      -- other config
+      -- The system_prompt type supports both a string and a function that returns a string. Using a function here allows dynamically updating the prompt with mcphub
+      system_prompt = function()
+        local hub = require("mcphub").get_hub_instance()
+        return hub:get_active_servers_prompt()
+      end,
+      -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+      custom_tools = function()
+        return {
+          require("mcphub.extensions.avante").mcp_tool(),
+        }
+      end,
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
@@ -60,8 +99,8 @@ return {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
-      -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "echasnovski/mini.icons",
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
