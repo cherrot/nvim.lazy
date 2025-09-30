157eeeacaac97e22cf7e769b854a0e9723899df0
cherrot <cherrotluo@gmail.com>
2025-09-30 22:24:13 +0800

    chore: revert ui workaround

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -7,16 +7,6 @@ return {
     },
   },
   {
-    -- workaround for LazyVim breaking change. see https://github.com/LazyVim/LazyVim/pull/6354#issuecomment-3202799735
-    "akinsho/bufferline.nvim",
-    init = function()
-      local bufline = require("catppuccin.groups.integrations.bufferline")
-      function bufline.get()
-        return bufline.get_theme()
-      end
-    end,
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {
