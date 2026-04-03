fc74dffbc88084857863e84dfcb78e121806520d
Luo Zheng <luozheng@moonshot.ai>
2026-04-03 16:13:25 +0800

    feat(ui): add pokemon and jj status on welcome page.
    prevent showing `[Process exited 0]` in terminal

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -37,4 +37,47 @@ return {
       },
     },
   },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        -- your dashboard configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        sections = {
+          { section = "header" },
+          {
+            {
+              section = "terminal",
+              pane = 2,
+              cmd = 'echo "\n\n\n\n\n"; pokemon-go-colorscripts -r --no-title',
+              random = 10,
+              indent = 4,
+              height = 22,
+              auto_close = false,
+              enabled = function()
+                return vim.o.columns > 127
+              end,
+            },
+            { section = "keys", gap = 1, padding = 1 },
+            { section = "startup" },
+          },
+          {
+            section = "terminal",
+            pane = 2,
+            icon = " ",
+            title = "jj Status",
+            cmd = "jj st --no-pager",
+            enabled = function()
+              return vim.o.columns > 127 and vim.fn.isdirectory(".jj") == 1
+            end,
+            height = 12,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 2,
+          },
+        },
+      },
+    },
+  },
 }
